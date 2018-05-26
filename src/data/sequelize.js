02e5f1a709f164554c79c5806689133596673a92
Minh Phan <phanminh65@gmail.com>
2018-05-26 14:23:59 +0700

    first commit

---
+++ src/data/sequelize.js
@@ -0,0 +1,13 @@
+
+
+import Sequelize, { Op } from 'sequelize';
+import config from '../config';
+
+const sequelize = new Sequelize(config.databaseUrl, {
+  operatorsAliases: Op,
+  define: {
+    freezeTableName: true,
+  },
+});
+
+export default sequelize;
